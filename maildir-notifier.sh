8fd7bcc71cf14afb6d7beb36d22f5941b5022e4a
Larhard <larhard@gmail.com>
2015-04-25 22:38:49 +0200

    launcher: detect if local rc file exists

--- maildir-notifier.sh
+++ maildir-notifier.sh
@@ -3,5 +3,11 @@
 PYTHON="/bin/env python3"
 PROGNAME="mail_notify.py"
 BASEDIR=$(dirname $(readlink -e "$0"))
+LOCAL_CONFIG_FILE="$HOME/.maildir-notifier.rc"
+ARGS=""
 
-$PYTHON "$BASEDIR/$PROGNAME" "$@"
+if [ -e "$LOCAL_CONFIG_FILE" ]; then
+    ARGS="$ARGS -c $LOCAL_CONFIG_FILE"
+fi
+
+$PYTHON "$BASEDIR/$PROGNAME" ${ARGS} "$@"
