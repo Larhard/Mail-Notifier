313d2086f8d1554aec96cb94bc5e5cfec21f4b03
Larhard <larhard@gmail.com>
2015-04-25 22:16:50 +0200

    config: add sounds support

--- notify.py
+++ notify.py
@@ -1,4 +1,6 @@
 import notify2
+import subprocess
+import config
 
 
 def init(program):
@@ -7,3 +9,5 @@ def init(program):
 
 def send(title, message):
     notify2.Notification(title, message).show()
+    if config.sounds and config.sound_cmd:
+        subprocess.call(config.sound_cmd, shell=True)
