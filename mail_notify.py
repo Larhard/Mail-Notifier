cdf98bcebab0f93f9a233cdfe469284e9a1c0f27
Larhard <larhard@gmail.com>
2015-04-25 21:59:18 +0200

    mail_notify: add config override support

--- mail_notify.py
+++ mail_notify.py
@@ -4,12 +4,18 @@ import argparse
 import sys
 
 import watcher
+import config
 
 
 def main(argv):
     parser = argparse.ArgumentParser('mail_notify')
     parser.add_argument('maildir')
+    parser.add_argument('--config', '-c', help="configuration file")
     args = parser.parse_args(argv)
+
+    config.override('default_config')
+    if args.config:
+        config.override(args.config)
 
     watcher.watch_maildir(args.maildir)
 
